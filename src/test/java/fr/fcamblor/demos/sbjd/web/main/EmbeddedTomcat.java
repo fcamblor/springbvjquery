d31bfc50a5d677edea584346149fd9ad6d05d58d
Frédéric Camblor <fcamblor@gmail.com>
2012-05-20 18:34:53 +0200

    extracted tomcat embed code into EmbeddedTomcat class

---
+++ src/test/java/fr/fcamblor/demos/sbjd/web/main/EmbeddedTomcat.java
@@ -0,0 +1,60 @@
+package fr.fcamblor.demos.sbjd.web.main;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+import javax.servlet.ServletException;
+import java.io.File;
+
+/**
+ * @author fcamblor
+ */
+public class EmbeddedTomcat {
+    int webPort;
+    Tomcat tomcat;
+
+    public EmbeddedTomcat(){
+        this(resolveDefaultWebPort());
+    }
+
+    public EmbeddedTomcat(int webPort){
+        Tomcat tomcat = new Tomcat();
+
+        tomcat.setPort(webPort);
+        this.webPort = webPort;
+
+        String webappDirLocation = "src/main/webapp/";
+        try {
+            tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        } catch (ServletException e) {
+            throw new RuntimeException("Failed adding webapp to embedded tomcat", e);
+        }
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+
+        this.tomcat = tomcat;
+    }
+
+    public void start() throws LifecycleException {
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+
+    public void stop() throws LifecycleException {
+        tomcat.stop();
+    }
+
+    public int getWebPort() {
+        return webPort;
+    }
+
+    private static int resolveDefaultWebPort() {
+        //The port that we should run on can be set into an environment variable
+        //Look for that variable and default to 8080 if it isn't there.
+        String webPortStr = System.getenv("web.port");
+        if(webPortStr == null || webPortStr.isEmpty()) {
+            return 8080;
+        } else {
+            return Integer.valueOf(webPortStr);
+        }
+    }
+}
--- src/test/java/fr/fcamblor/demos/sbjd/web/main/Main.java
+++ src/test/java/fr/fcamblor/demos/sbjd/web/main/Main.java
@@ -10,23 +10,6 @@ import java.io.File;
  */
 public class Main {
 	public static void main(String[] args) throws Exception {
-        String webappDirLocation = "src/main/webapp/";
-
-        Tomcat tomcat = new Tomcat();
-
-        //The port that we should run on can be set into an environment variable
-        //Look for that variable and default to 8080 if it isn't there.
-        String webPort = System.getenv("web.port");
-        if(webPort == null || webPort.isEmpty()) {
-            webPort = "8080";
-        }
-
-        tomcat.setPort(Integer.valueOf(webPort));
-
-        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
-        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
-
-        tomcat.start();
-        tomcat.getServer().await();
+        new EmbeddedTomcat().start();
 	}
 }
