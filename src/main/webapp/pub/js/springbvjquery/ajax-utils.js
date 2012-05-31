233300a3c182e056b8c233d92d66baa54a97e865
Frédéric Camblor <fcamblor@gmail.com>
2012-06-01 01:01:38 +0200

    when status code 412 detected, displaying spring binding errors only if responseText is a parsable JSON content

--- src/main/webapp/pub/js/springbvjquery/ajax-utils.js
+++ src/main/webapp/pub/js/springbvjquery/ajax-utils.js
@@ -1,4 +1,4 @@
-// this prevent jquery to add square brackets [] at the end of param name when they have multiple values,
+// This prevent jquery to add square brackets [] at the end of param name when they have multiple values,
 // which is breaking spring binding.
 $.ajaxSettings.traditional = true;
 /* Declaring global ajax settings for $.ajax() calls */
@@ -7,10 +7,13 @@ $.ajaxSetup({
     cache: false
 });
 
-
-
+/**
+ * Overriding default behaviour for ajax errors : looking at jquery XMLHttpRequest's status
+ * and, if it is the special 412 (Precondition failed) status _and_ response is a JSON representation,
+ * then call $.displaySpringErrors()
+ */
 $(document).ajaxError(function(event, jqXHR, settings, exception){
-    if(jqXHR.status === 412){
+    if(jqXHR.status === 412 && isJSON(jqXHR.responseText)){
         var springBindingResults = $.parseJSON(jqXHR.responseText);
         $.displaySpringErrors(springBindingResults);
     }
--- src/main/webapp/pub/js/springbvjquery/js-utils.js
+++ src/main/webapp/pub/js/springbvjquery/js-utils.js
@@ -71,3 +71,16 @@
     };
 
 })( jQuery );
+
+//Test if a str is a JSON
+function isJSON(str) {
+    if (blank(str)) return false;
+    str = str.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@');
+    str = str.replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']');
+    str = str.replace(/(?:^|:|,)(?:\s*\[)+/g, '');
+    return (/^[\],:{}\s]*$/).test(str);
+};
+
+function blank(str) {
+    return /^\s*$/.test(this);
+};
